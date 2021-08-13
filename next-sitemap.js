386e7b2fafe0b4e7175078d823f7a903571255d3
supardiGunawan726 <xblade131@gmail.com>
2021-08-13 21:19:24 +0700

    add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteUrl: "https://pardiwan.my.id",
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // =>  this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
+};
